e26eeb1ea3075a4391ab8c5b24a4b62fcf730d6a
DEVELOPERCOM\quaj6 <quaj628@gmail.com>
2023-10-08 00:42:10 +0900

    commit

--- board/src/main/java/org/zerock/aop/LogAdvice.java
+++ board/src/main/java/org/zerock/aop/LogAdvice.java
@@ -1,10 +1,6 @@
 package org.zerock.aop;
 
-import java.util.Arrays;
 
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.AfterThrowing;
-import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
